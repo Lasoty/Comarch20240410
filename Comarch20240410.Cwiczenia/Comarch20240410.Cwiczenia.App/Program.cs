ccf6ff0afac7c260ea569e7a79750e11051fcc06
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2024-04-10 14:01:35 +0200

    Initial

---
+++ Comarch20240410.Cwiczenia/Comarch20240410.Cwiczenia.App/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Comarch20240410.Cwiczenia.App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
