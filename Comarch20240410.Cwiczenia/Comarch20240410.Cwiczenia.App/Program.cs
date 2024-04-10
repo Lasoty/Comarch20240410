e74e53bc0da52902a8a1d3ee735a39cd0a944ad2
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2024-04-10 16:08:45 +0200

    instrukcje warunkowe

--- Comarch20240410.Cwiczenia/Comarch20240410.Cwiczenia.App/Program.cs
+++ Comarch20240410.Cwiczenia/Comarch20240410.Cwiczenia.App/Program.cs
@@ -4,15 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Jak masz na imię?");
-            string? imie = Console.ReadLine();
+            Console.WriteLine("KALKULATOR v1.0.");
+            Console.WriteLine("MENU: ");
+            Console.WriteLine(" 1. Dodawanie");
+            Console.WriteLine(" 2. Odejmowanie");
 
-            Console.WriteLine("Ile masz lat?");
-            string? wiekTekst = Console.ReadLine();
-            int wiekLiczba = int.Parse(wiekTekst!);
+            Console.Write("Wybierz opcję: ");
 
-            string wynik = "Witaj " + imie + ", masz " + wiekLiczba + " lat.";
-            Console.WriteLine(wynik);
+            bool czyPoprawna = int.TryParse(Console.ReadLine(), out int wybor);
+
+            if (czyPoprawna == true)
+            {
+                switch (wybor)
+                {
+                    case 1:
+                        Console.WriteLine("Dodawanie.");
+                        break;
+                    case 2:
+                        Console.WriteLine("Odejmowanie.");
+                        break;
+
+                    default:
+                        Console.WriteLine("Podana wartość jest poza zakresem.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Podana wartość jest nieprawidłowa");
+            }
         }
     }
 }
