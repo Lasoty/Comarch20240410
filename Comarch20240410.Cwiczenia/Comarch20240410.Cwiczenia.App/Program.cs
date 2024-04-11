150a8401afe76af167178850af18d71f498e3248
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2024-04-11 10:37:59 +0200

    Metody

--- Comarch20240410.Cwiczenia/Comarch20240410.Cwiczenia.App/Program.cs
+++ Comarch20240410.Cwiczenia/Comarch20240410.Cwiczenia.App/Program.cs
@@ -1,38 +1,66 @@
-﻿namespace Comarch20240410.Cwiczenia.App
+﻿using Comarch20240410.Cwiczenia.App.Services;
+
+namespace Comarch20240410.Cwiczenia.App
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("KALKULATOR v1.0.");
-            Console.WriteLine("MENU: ");
-            Console.WriteLine(" 1. Dodawanie");
-            Console.WriteLine(" 2. Odejmowanie");
-
-            Console.Write("Wybierz opcję: ");
-
-            bool czyPoprawna = int.TryParse(Console.ReadLine(), out int wybor);
-
-            if (czyPoprawna == true)
+            bool czyKontynuacja = true;
+            do
             {
-                switch (wybor)
+                Console.Clear();
+
+                Console.WriteLine("KALKULATOR v1.0.");
+                Console.WriteLine("MENU: ");
+                Console.WriteLine(" 1. Dodawanie");
+                Console.WriteLine(" 2. Odejmowanie");
+
+                Console.Write("Wybierz opcję: ");
+
+                bool czyPoprawna = int.TryParse(Console.ReadLine(), out int wybor);
+
+                if (czyPoprawna == true)
                 {
-                    case 1:
-                        Console.WriteLine("Dodawanie.");
-                        break;
-                    case 2:
-                        Console.WriteLine("Odejmowanie.");
-                        break;
+                    Calculator calculator = new Calculator();
+                    int x, y, wynik;
 
-                    default:
-                        Console.WriteLine("Podana wartość jest poza zakresem.");
-                        break;
+                    switch (wybor)
+                    {
+                        case 1:
+                            Console.Write("Podaj X: ");
+                            x = int.Parse(Console.ReadLine());
+                            Console.Write("Podaj Y: ");
+                            y = int.Parse(Console.ReadLine());
+                            wynik = calculator.Add(x, y);
+                            Console.WriteLine($"Wynik dodawania {x} oraz {y} to {wynik}.");
+                            break;
+                        case 2:
+                            Console.Write("Podaj X: ");
+                            x = int.Parse(Console.ReadLine());
+                            Console.Write("Podaj Y: ");
+                            y = int.Parse(Console.ReadLine());
+                            wynik = calculator.Subtract(x, y);
+                            Console.WriteLine($"Wynik odejmowania {x} oraz {y} to {wynik}.");
+                            break;
+
+                        default:
+                            Console.WriteLine("Podana wartość jest poza zakresem.");
+                            break;
+                    }
                 }
-            }
-            else
-            {
-                Console.WriteLine("Podana wartość jest nieprawidłowa");
-            }
+                else
+                {
+                    Console.WriteLine("Podana wartość jest nieprawidłowa");
+                }
+
+                Console.ReadLine();
+
+                Console.Clear();
+                Console.Write("Czy chcesz wykonać nową operację? [T | n]: ");
+                var wyborNaKoniec = Console.ReadKey();
+                czyKontynuacja = wyborNaKoniec.Key != ConsoleKey.N;
+            } while (czyKontynuacja);
         }
     }
 }
---
+++ Comarch20240410.Cwiczenia/Comarch20240410.Cwiczenia.App/Services/Calculator.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Comarch20240410.Cwiczenia.App.Services
+{
+    public class Calculator
+    {
+        public int Add(int x, int y)
+        {
+            int wynik = x + y;
+            return wynik;
+        }
+
+        internal int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+    }
+}
