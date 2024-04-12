db50b539119bb073e9388b3e156bfc35c9044cb2
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2024-04-12 10:16:38 +0200

    Initial Bibliotekarz Solution

---
+++ Bibliotekarz/Bibliotekarz.Server/Controllers/BooksController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Bibliotekarz.Server.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class BooksController : ControllerBase
+{
+    public BooksController()
+    {
+        
+    }
+
+    [HttpGet("All")]
+    public async Task<IActionResult> GetAllBooks()
+    {
+
+        return Ok();
+    }
+}
