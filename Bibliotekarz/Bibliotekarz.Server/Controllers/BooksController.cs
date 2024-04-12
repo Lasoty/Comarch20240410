d9e2ae0224bd8293d3e46257fa4fa13bf3cbcea8
Leszek Lewandowski <leszek.lewandowski@dnv.com>
2024-04-12 11:16:15 +0200

    Kontroler i endpoint

--- Bibliotekarz/Bibliotekarz.Server/Controllers/BooksController.cs
+++ Bibliotekarz/Bibliotekarz.Server/Controllers/BooksController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Bibliotekarz.Shared.ModelDto;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Bibliotekarz.Server.Controllers;
 
@@ -8,13 +9,60 @@ public class BooksController : ControllerBase
 {
     public BooksController()
     {
-        
+
     }
 
     [HttpGet("All")]
     public async Task<IActionResult> GetAllBooks()
     {
+        List<BookDto> fakeData = GenerateFakeData();
+        return Ok(fakeData);
+    }
 
-        return Ok();
+    private List<BookDto> GenerateFakeData()
+    {
+        //var fakeData = new List<BookDto>();
+        //fakeData.Add(new BookDto());
+        //BookDto book = fakeData[0];
+        //fakeData.RemoveAt(0);
+        //fakeData.Remove(book);
+
+        //var fakeData = new Dictionary<string, BookDto>();
+        //fakeData.Add("ISBN_374629873469287364", new BookDto());
+        //BookDto book = fakeData["ISBN_374629873469287364"];
+        //fakeData.Remove("ISBN_374629873469287364");
+
+        //var fakeData = new Queue<BookDto>(); //FIFO
+        //fakeData.Enqueue(new BookDto());
+        //var book = fakeData.Dequeue();
+        //book = fakeData.Peek();
+
+        //var fakeData = new Stack<BookDto>(); //FILO
+        //fakeData.Push(new BookDto());
+        //BookDto book = fakeData.Pop();
+
+        List<BookDto> books =
+            [
+                new BookDto
+                {
+                    Id = 1,
+                    Author = "Leszek Lewandowski",
+                    Title = "Programowa",
+                    PageCount = 1,
+                    IsBorrowed = true,
+                    BorrowerFirstName = "Adam",
+                    BorrowerLastName = "Nowak"
+                },
+                new BookDto
+                {
+                    Id = 2,
+                    Author = "John Sharp",
+                    Title = "ASP.NET Core 2.0",
+                    PageCount = 1,
+                    IsBorrowed = false
+                }
+            ];
+
+        return books;
     }
 }
---
+++ Bibliotekarz/Bibliotekarz.Shared/ModelDto/BookDto.cs
@@ -0,0 +1,19 @@
+﻿namespace Bibliotekarz.Shared.ModelDto;
+
+public class BookDto
+{
+    public int Id { get; set; }
+
+    public required string Title { get; set; }
+
+    public string Author { get; set; }
+
+    public int PageCount { get; set; }
+
+    public bool IsBorrowed { get; set; }
+
+    public string BorrowerFirstName { get; set; }
+
+    public string BorrowerLastName { get; set; }
+
+}
